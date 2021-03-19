43a3d8da1d8d67a78ff22cda2fcad744298aa6d4
Alex <expresproject@gmail.com>
2021-03-19 02:35:11 +0200

    add video and responsive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-  
-})
+$(document).ready(function () {
+  $('#app').vide({
+    mp4: 'video/video.mp4',
+    webm: 'video/video.webm',
+    ogv: 'video/video.ogv',
+    poster: 'img/cover-image.jpeg'
+  });
+});
